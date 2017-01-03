e61b18707f437dac48cda9f4a837ab5c64b48cd9
Radu-Dunarentu <dunarentu.radu@gmail.com>
2017-01-03 23:44:22 +0200

    quick fix

--- src/app/tesla-battery/models/stat.interface.ts
+++ src/app/tesla-battery/models/stat.interface.ts
@@ -1,3 +1,7 @@
 /**
  * Created by Radu on 12/31/2016.
  */
+export interface Stat {
+  model: string,
+  miles: number
+}
--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -4,11 +4,18 @@
 /*
  * tesla-battery.module.ts
  */
-// containers
-import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+
+// containers
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+
+// components
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
+import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
+import { TeslaWheelsComponent } from './components/tesla-wheels/tesla-wheels.component';
 
 // services
 import { BatteryService } from './tesla-battery.service';
@@ -16,18 +23,22 @@ import {TeslaCarComponent} from "./components/tesla-car/tesla-car-component";
 
 @NgModule({
   declarations: [
-    // registering our container component
     TeslaBatteryComponent,
-    TeslaCarComponent
+    TeslaCarComponent,
+    TeslaStatsComponent,
+    TeslaCounterComponent,
+    TeslaClimateComponent,
+    TeslaWheelsComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
   providers: [
-    // add the service to our sub-module
     BatteryService
   ],
-  exports: [TeslaBatteryComponent]
+  exports: [
+    TeslaBatteryComponent
+  ]
 })
 export class TeslaBatteryModule {}
